be68974cf507dc2e63ce4f503186227a394f7ef4
midnightrain9 <118661352+midnightrain9@users.noreply.github.com>
2024-04-17 22:39:25 +0530

    Update Peak element.cpp

--- SEARCHING/Peak element.cpp
+++ SEARCHING/Peak element.cpp
@@ -1,3 +1,4 @@
+// in this question it is very necessary to check conditions before hand
 // first approach checks the corner condition inside the while loop only
 class Solution {
 public:
@@ -20,4 +21,42 @@ public:
     return -1;
     }
 
+};
+
+// second approach checks for the corner conditions out of the loop
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int low = 1, high = nums.size()-2;
+        int peak =-1;
+        if (nums.size() == 1){
+           return 0;
+        }
+        if (nums[0]> nums[1]){ // checking for the corner conditions seperately before entering the loop
+            return 0;
+        }
+
+        if (nums[n-1]> nums[n-2]){
+            return n-1; 
+        }
+      while (low<=high){
+         int mid= low +(high - low)/2;
+          if (nums[mid-1]<nums[mid]&& nums[mid+1]<nums[mid] ){
+              peak = mid;
+              break;
+          }
+          
+          else if (nums[mid] > nums[mid +1]) {
+           high  = mid -1;
+          }
+          else {
+              low = mid+1;
+
+          }
+      }
+
+          return peak;
+    }
 };
