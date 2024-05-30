8495fcab44e87fba20d35a693656da7b9c5b8f8f
midnightrain9 <118661352+midnightrain9@users.noreply.github.com>
2024-05-30 22:30:03 +0530

    Create Palindrome.cpp

---
+++ STRINGS/Palindrome.cpp
@@ -0,0 +1,52 @@
+// my initial thought process was to take two pointers one pointing at the beginning and other at the end and then to check if the string is of even length or not
+bool isPalindrome (string str){
+    int n = str.length ();
+    int start = 0;
+    int end = n-1;
+    if (n%2==0){
+        while (start <n/2){
+            if (str[start] == str [end]){
+                start++;
+                end--;
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    else {
+        while (start < (n/2)-1){
+            if (str[start] == str[end]){
+                start++;
+                end--;
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+        
+    }
+    return true;
+  
+} // Takes theta  n time not O(n).
+
+// Better approach - simply take two pointers one pointing to the beginning and other to the end of the array and then do start ++ and end-- till start < end . 
+// covers both the cases for even and odd length strings
+bool isPalindrome (string str){
+    int n = str.length ();
+    int start = 0;
+    int end = n-1;
+    while (start<end){
+        if (str[start]==str[end]){
+            start++;
+            end--;
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+
