34d21271501c4b248b2142159e7ecc1e02945676
Divya <shukladivya7298@gmail.com>
2024-08-25 16:51:14 +0530

    Added a new folder LINKED_LIST and created the file InsertAtAllPos.cpp

---
+++ LINKED_LIST/InsertAtAllPos.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int x){
+        this->data = x;
+        this->next = NULL;
+    }
+};
+void InsertAtHead(Node *&head, int d){
+    Node *temp = new Node(d);
+    temp->next = head->next;
+    head = temp;
+}
+void InsertAtTail (Node * &tail , int d){
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void print(Node *&head){
+    Node *temp = head;
+    while (temp != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+        
+    }
+}
+
+void insertAtPos(Node *&head,Node *&tail, int pos, int d){
+
+if(pos ==1){
+    InsertAtHead(head, d);
+    return;
+}
+    int cnt = 1;
+    Node *n = new Node(d);
+    Node *temp = head;
+    while (cnt < pos-1){
+        temp = temp->next;
+        cnt++;
+    }
+    if(temp->next == NULL){
+        InsertAtTail(tail, d);
+        return;
+    }
+    n->next = temp->next;
+    temp->next = n;
+}
+
+
+int main (){
+    Node *node1  = new Node(10);
+    Node *head = node1;
+    Node *tail = node1;
+
+    InsertAtTail(tail, 20);
+    InsertAtTail(tail, 30);
+    InsertAtTail(tail, 40);
+    insertAtPos(head,tail, 5, 50);
+    print(head);
+}
